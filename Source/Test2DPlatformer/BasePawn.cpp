7527306f9a3e7432a1e177c6feae880c2e4b953d
Valerio Santinelli <santinelli@altralogica.it>
2017-02-26 19:01:15 +0100

    VSCode and YouCompleteMe settings.

--- Source/Test2DPlatformer/BasePawn.cpp
+++ Source/Test2DPlatformer/BasePawn.cpp
@@ -23,7 +23,6 @@ void ABasePawn::BeginPlay()
 void ABasePawn::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
-
 	LastTimeMult = TimeMult;
 	ActualDeltaTime = DeltaTime * TimeRate;
 	ComputedDeltaTime = FMath::Min(ActualDeltaTime, 0.01666667f * (TimeRate + 0.008333334f));
