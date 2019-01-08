730243599b100f4fea4fc95fc4607fc35a8752f1
Valerio Santinelli <santinelli@altralogica.it>
2019-01-08 16:25:09 +0100

    Removed build files

--- cmake-build-debug/CMakeFiles/Test2DPlatformerEditor-Development-CLEAN.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Test2DPlatformerEditor-Development-CLEAN"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Test2DPlatformerEditor-Development-CLEAN.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/Test2DPlatformerEditor-Development.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Test2DPlatformerEditor-Development"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Test2DPlatformerEditor-Development.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
